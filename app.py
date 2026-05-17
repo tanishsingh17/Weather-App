fa5036ea56e618266fa7937d1e977e353033edb7
Tanish Singh Thakur <tanish.singhthakur123@gmail.com>
2026-05-17 17:00:25 +0530

    Updated weather app for deployment

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from dotenv import load_dotenv
+import os
 import requests
 import json
 
-app = Flask(__name__)
-app.secret_key = "supersecretkey"
+load_dotenv()
 
-API_KEY = "8eb15a1ae15a5a52ff66bfa0b6fed138"
+app = Flask(__name__)
+
+app.secret_key = os.getenv("SECRET_KEY", "fallbacksecret")
+API_KEY = os.getenv("API_KEY")
 
 with open("cities.json", "r") as f:
     cities_data = json.load(f)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+
     if request.method == "POST":
-        country = request.form["country"]
-        city = request.form["city"]
+
+        country = request.form.get("country")
+        city = request.form.get("city")
+
+        if not country or not city:
+            session["weather_data"] = {
+                "error": "Please select both country and city"
+            }
+
+            return redirect(url_for("index"))
 
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={API_KEY}&units=metric"
 
         try:
-            response = requests.get(url)
+
+            response = requests.get(url, timeout=5)
             data = response.json()
 
             if response.status_code == 200:
+
                 session["weather_data"] = {
                     "city": data["name"],
                     "country": data["sys"]["country"],
@@ -33,18 +49,29 @@ def index():
                     "description": data["weather"][0]["description"].title(),
                     "icon": data["weather"][0]["icon"]
                 }
-            else:
-                session["weather_data"] = {"error": "City not found"}
 
-        except:
-            session["weather_data"] = {"error": "Internet or API error"}
+            else:
+
+                session["weather_data"] = {
+                    "error": "City not found"
+                }
+
+        except Exception as e:
+
+            session["weather_data"] = {
+                "error": "Internet or API error"
+            }
 
         return redirect(url_for("index"))
 
     weather_data = session.pop("weather_data", None)
 
-    return render_template("index.html", weather=weather_data, cities_data=cities_data)
+    return render_template(
+        "index.html",
+        weather=weather_data,
+        cities_data=cities_data
+    )
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
